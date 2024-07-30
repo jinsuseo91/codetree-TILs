8822c9ef66bbb703980b2c9df2a2aa769d958d55
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-07-31 00:17:09 +0900

    Title: 행복한 수열의 개수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+d = []
+for _ in range(n):
+    d.append(list(map(int, input().split())))
+cnt = 0
+#행
+for i in range(n):
+    hap = 1
+    prev_num = d[i][0] 
+    for j in range(1, n):
+        if prev_num == d[i][j]:
+            hap += 1
+        else:
+            hap = 1
+            prev_num = d[i][j]
+    if hap >= m:
+        cnt += 1
+#열
+for j in range(n):
+    for k in range(m):
+        hap = 0
+        if d[i][0] != d[i][k]:
+            break
+        else:
+            hap += 1
+    if hap >= m:
+        cnt += 1
+for j in range(n):
+    hap = 0
+    prev_num = d[0][j] 
+    for i in range(1, n):
+        if prev_num == d[i][j]:
+            hap += 1
+        else:
+            hap = 1
+            prev_num = d[i][j]
+    if hap >= m:
+        cnt += 1
+print(cnt)
