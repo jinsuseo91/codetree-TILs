c03ce814ad8d8a2986c4b34e981be76e38a900bf
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-07-31 00:53:22 +0900

    Title: 행복한 수열의 개수, Time: 164ms, Memory: 73MB, Status: Passed - CodeTree

--- 240731/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240731/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,30 +1,17 @@
 n, m = map(int, input().split())
 d = []
-for _ in range(n):
-    d.append(list(map(int, input().split())))
 cnt = 0
-#행
-for i in range(n):
-    hap = 1
-    prev_num = d[i][0] 
-    for j in range(1, n):
-        if prev_num == d[i][j]:
-            hap += 1
-        else:
-            hap = 1
-            prev_num = d[i][j]
-    if hap >= m:
-        cnt += 1
+for _ in range(n):
+    row = list(map(int, input().split()))
+    d.append(row)
+    for i in range(n - m + 1):
+        if row[i:i+m].count(row[i]) == m:
+            cnt += 1
+            break
 #열
-for j in range(n):
-    hap = 1
-    prev_num = d[0][j] 
-    for i in range(1, n):
-        if prev_num == d[i][j]:
-            hap += 1
-        else:
-            hap = 1
-            prev_num = d[i][j]
-    if hap >= m:
-        cnt += 1
+for j in zip(*d):
+    for i in range(n-m+1):
+        if j[i:i+m].count(j[i]) == m:
+            cnt += 1
+            break
 print(cnt)
