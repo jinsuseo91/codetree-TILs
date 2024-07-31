409cde6fc6be123c05e30df63d79707df349d7ea
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-07-31 16:49:55 +0900

    Title: 컨베이어 벨트, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240731/컨베이어 벨트/conveyor-belt.py
+++ 240731/컨베이어 벨트/conveyor-belt.py
@@ -2,10 +2,11 @@ n, t = map(int, input().split())
 array1 = list(map(int, input().split()))
 array2 = list(map(int, input().split()))
 array = array1 + array2
-for i in range(t):
+# for i in range(2*n):
+for j in range(t):
     temp = array[2*n-1]
     for k in range(2*n-1, 0, -1):
         array[k] = array[k-1]
-        array[0] = temp
+    array[0] = temp
 print(*array[0:n])
 print(*array[n:6])
