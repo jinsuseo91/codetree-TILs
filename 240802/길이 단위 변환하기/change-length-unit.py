5a47d8aba8aea7f6326ceb588928bec631d449bf
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-02 00:27:07 +0900

    Title: 길이 단위 변환하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240802/길이 단위 변환하기/change-length-unit.py
+++ 240802/길이 단위 변환하기/change-length-unit.py
@@ -4,5 +4,5 @@ a = 9.2
 b = 1.3
 result1 = a * f
 result2 = m * b
-print(f"9.2ft=%.f %{result1}cm")
-print(f"1.3ft=%.f %{result2}cm"")
+print(f"9.2ft=%.1f %result1cm")
+print(f"1.3ft=%.1f %result2cm"")
