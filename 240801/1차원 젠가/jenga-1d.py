00dcdd483acc07014e6b9d88afa95847ffca8b0a
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-01 13:31:39 +0900

    Title: 1차원 젠가, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/1차원 젠가/jenga-1d.py
+++ 240801/1차원 젠가/jenga-1d.py
@@ -3,39 +3,26 @@ blank = 0
 arr = [int(input()) for _ in range(n)]
 remo = [map(int, input().split()) for _ in range(2)]
 
-temp = [0] * n
-
-for i in range(len(remo)):
-    if i == 0:
-        s1, e1 = remo[i]
-        s1 -= 1
-        e1 -= 1
-    else:
-        s2, e2 = remo[i]
-        s2 -= 1
-        e2 -= 1
-
+tempRow = n-1
 # 제거하기
 def dele(arr, s, e):
-    tempRow = n-1
-    for i in range(s1, e1+1):
-        arr[i] =  0
+    s -= 1
+    e -= 1
 
-    for i in range(n-1, -1, -1):
-        if arr[i] != blank:
-            temp[tempRow] = arr[i]
-            tempRow -= 1
+    for i in range(s, e + 1):
+        arr[i] =  0
+    
+    temp = [x for x in arr if x != 0]
 
     return temp
 
+s1, e1 = remo[0]
 arr = dele(arr, s1, e1)
+
+s2, e2 = remo[1]
 arr = dele(arr, s2, e2)
 
-
-
-a = sum(1 for x in arr if x != 0)
-print(a)
+print(len(arr))
 
 for i in arr:
-    if i != 0:
-        print(i)
+    print(i)
