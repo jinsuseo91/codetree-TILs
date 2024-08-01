71f8bc8616a1e6c535ead97c54556a97f4b93b0d
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-02 00:31:23 +0900

    Title: 길이 단위 변환하기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/길이 단위 변환하기/change-length-unit.py
+++ 240802/길이 단위 변환하기/change-length-unit.py
@@ -4,5 +4,5 @@ a = 9.2
 b = 1.3
 result1 = a * f
 result2 = m * b
-print(f"9.2ft={result1:.1f}cm")
-print(f"1.3mi={result2:.1f}cm")
+print(f"9.2ft = {result1:.1f}cm")
+print(f"1.3mi = {result2:.1f}cm")
