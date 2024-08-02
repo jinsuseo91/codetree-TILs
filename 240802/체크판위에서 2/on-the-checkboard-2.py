6592a4b83aff14e1caccb2a41ac503c144499d1b
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-02 22:18:41 +0900

    Title: 체크판위에서 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/체크판위에서 2/on-the-checkboard-2.py
+++ 240802/체크판위에서 2/on-the-checkboard-2.py
@@ -1,20 +1,13 @@
-R, C = map(int, input().split())
+n, m = map(int, input().split())
 
-arr = [list(input().split()) for _ in range(R)]
+arr = [list(input().split()) for _ in range(n)]
 
-x, y = 0, 0
-c_color = arr[x][y]
 cnt = 0
-stop = 0
-def in_range(i, j):
-    return 0 <= i < R and 0 <= j < C
-
-for i in range(R):
-    for j in range(C):
-        if in_range(i, j) and i >= x + 1 and j >= y + 1 and c_color != arr[i][j]:
-            x, y = i, j
-            c_color = arr[i][j]
-            stop += 1
-    if stop == 3 and (x, y) == (R-1, C-1):
-        cnt += 1
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(i + 1, n - 1):
+            for l in range(j + 1, m - 1):
+                # 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[n - 1][m - 1]:
+                    cnt += 1
 print(cnt)
