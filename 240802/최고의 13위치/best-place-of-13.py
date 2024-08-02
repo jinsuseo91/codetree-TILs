cc66527d67a7d637c33e9c8e5fafd914f167836e
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-02 19:51:48 +0900

    Title: 최고의 13위치, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240802/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,10 @@
+n = int(input())
+array = []
+cnt = 0
+max_cnt = 0
+arr = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n - 2):
+        max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2])
+
+print(max_cnt)
