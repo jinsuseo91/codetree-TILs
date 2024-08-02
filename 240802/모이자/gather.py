39408d3a673a7de009c6edaeb3d7758ec6719157
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-02 17:09:12 +0900

    Title: 모이자, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/모이자/gather.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+min_sum = 1000
+sum_ = 0
+#n은 방문할 집 번호
+for i in range(n):
+    sum_ = 0
+    #k는 이동하는 집의 인덱스
+    for k in range(n):
+        sum_ += arr[k] * abs(i - k)
+    min_sum = min(min_sum, sum_)
+
+print(min_sum)
