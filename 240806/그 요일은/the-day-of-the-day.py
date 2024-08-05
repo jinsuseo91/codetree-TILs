697a9b8cd243150be21c8957d20c9789975c8851
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-06 02:04:13 +0900

    Title: 그 요일은, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/그 요일은/the-day-of-the-day.py
+++ 240806/그 요일은/the-day-of-the-day.py
@@ -1,18 +1,39 @@
+# 입력 받기
 m1, d1, m2, d2 = map(int, input().split())
 day = input()
-days = 0
+
+# 각 달의 일수
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# 시작 요일을 Monday로 설정하여 요일 목록 생성
 day_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-for i in range(m1, m2):
-    days += num_of_days[i]
-days = days + d2 - d1 + 1
+# 전체 일수를 계산하는 함수
+def calculate_days(m1, d1, m2, d2):
+    days = 0
+    if m1 == m2:
+        days = d2 - d1 + 1
+    else:
+        days += num_of_days[m1] - d1 + 1
+        for i in range(m1 + 1, m2):
+            days += num_of_days[i]
+        days += d2
+    return days
 
-n = day_list.index(day)
+# 전체 일수 계산
+total_days = calculate_days(m1, d1, m2, d2)
 
-start_day_offset = (n + d1 - 1) % 7
+# 시작 요일의 인덱스 찾기
+start_day_index = day_list.index(day)
 
-total_day_occurrences = (days - (7 - start_day_offset) + 6) // 7 
+# 첫 번째 날짜가 시작 요일 기준으로 몇 번째 요일인지 계산
+start_day_offset = (start_day_index - (d1 % 7) + 7) % 7
+
+# 첫 번째 요일에 해당하는 날짜 계산
+first_occurrence_day = (7 - start_day_offset) % 7
+
+# 특정 요일이 범위 내에 몇 번 등장하는지 계산
+day_count = (total_days - first_occurrence_day + 6) // 7
 
 # 결과 출력
-print(total_day_occurrences)
+print(day_count)
