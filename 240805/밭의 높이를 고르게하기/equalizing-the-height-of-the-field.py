4c361adb4ef9d4d43374fc15d77153c6f4834cf7
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-05 23:40:26 +0900

    Title: 밭의 높이를 고르게하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,12 @@
+n, h, t = map(int, input().split())
+
+arr = list(map(int,input().split()))
+
+ans = 100000
+for i in range(n- t + 1):
+    cnt = 0
+    for j in range(i, i + t):
+        if 0 <= j <= n - 1:
+            cnt += abs(arr[j] - h)
+    ans = min(cnt, ans)
+print(ans)
