5ba21d91ac53faf3f529bc13a1ec3f581f98b399
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-06 02:05:50 +0900

    Title: 그 요일은, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/그 요일은/the-day-of-the-day.py
+++ 240806/그 요일은/the-day-of-the-day.py
@@ -2,40 +2,32 @@
 m1, d1, m2, d2 = map(int, input().split())
 day = input()
 
-# 각 달의 일수
+# 각 달의 일수 (2024년은 윤년이므로 2월이 29일까지 있습니다)
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# 시작 요일을 Monday로 설정하여 요일 목록 생성
+# 요일 리스트
 day_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-# 전체 일수를 계산하는 함수
-def calculate_days(m1, d1, m2, d2):
-    days = 0
-    if m1 == m2:
-        days = d2 - d1 + 1
-    else:
-        days += num_of_days[m1] - d1 + 1
-        for i in range(m1 + 1, m2):
-            days += num_of_days[i]
-        days += d2
-    return days
+# 시작 요일이 Monday로 설정된 2024년 1월 1일부터의 누적 일수를 계산하는 함수
+def days_from_start_of_year(month, day):
+    total_days = sum(num_of_days[:month]) + day
+    return total_days
 
-# 전체 일수 계산
-total_days = calculate_days(m1, d1, m2, d2)
+# 입력된 날짜 범위의 시작과 끝 일수를 계산
+start_days = days_from_start_of_year(m1, d1)
+end_days = days_from_start_of_year(m2, d2)
 
-# 시작 요일의 인덱스 찾기
-start_day_index = day_list.index(day)
+# 시작 날짜의 요일 인덱스를 계산
+start_day_index = (start_days - 1) % 7  # 2024년 1월 1일은 Monday
 
-# 첫 번째 날짜가 시작 요일 기준으로 몇 번째 요일인지 계산
-start_day_offset = (start_day_index - (d1 % 7) + 7) % 7
+# 특정 요일의 인덱스를 찾기
+target_day_index = day_list.index(day)
 
 # 특정 요일이 범위 내에 몇 번 등장하는지 계산
-day_count = 0
-current_day = start_day_offset
-for i in range(total_days):
-    if day_list[current_day % 7] == day:
-        day_count += 1
-    current_day += 1
+count = 0
+for day_count in range(start_days, end_days + 1):
+    if (day_count - 1) % 7 == target_day_index:
+        count += 1
 
 # 결과 출력
-print(day_count)
+print(count)
