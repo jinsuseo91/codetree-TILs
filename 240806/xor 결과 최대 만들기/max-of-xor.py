8b98a3136770ffb98416c2cf08bd4300485e057b
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-06 11:48:15 +0900

    Title: xor 결과 최대 만들기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240806/xor 결과 최대 만들기/max-of-xor.py
+++ 240806/xor 결과 최대 만들기/max-of-xor.py
@@ -7,26 +7,26 @@ memo = {}
 
 def calc():
     sum_ = 0
-    for i in ans:
-        sum_ ^= i
+    for i in range(1, m):
+        sum_ ^= ans[i]
     return sum_
 
-def choose(curr_num):
+def choose(curr_num, idx):
     global max_
 
-    key = tuple(ans)
-    if key in memo:
-        return memo[key]
+    # key = tuple(ans)
+    # if key in memo:
+    #     return memo[key]
 
     if curr_num == m + 1:
         result = calc()
         max_ = max(max_, result)
         return
 
-    for i in arr:
-        ans.append(i)
-        choose(curr_num + 1)
+    for i in range(idx + 1, n):
+        ans.append(arr[i])
+        choose(curr_num + 1, i)
         ans.pop()
 
-choose(1)
+choose(1, -1)
 print(max_)
