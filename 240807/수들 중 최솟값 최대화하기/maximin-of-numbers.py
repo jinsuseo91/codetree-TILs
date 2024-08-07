39d42c8e56e403cc5e04b067d0df99db7f3a8268
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-07 14:33:06 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 191ms, Memory: 28MB, Status: Passed - CodeTree

--- 240807/수들 중 최솟값 최대화하기/maximin-of-numbers.py
+++ 240807/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -1,40 +1,64 @@
 import sys
-n = int(sys.stdin.readline())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-visited =[False] * (n+1)
-numbers = []
-min_numbers = []
-min_numbers2 = []
 
-ans = -1
-def find_max(row) :
-    global ans
-    global min_numbers, min_numbers2
-    if row == n :
-        tmp_list = []
-        for i in range(n):
-            tmp_list.append(arr[numbers[i]][i])
-        tmp_ans = ans
+si = sys.stdin.readline
 
-        ans = max(ans, sum(tmp_list))
+# n 개의 칸을 색칠 1 <= n <= 10
+n = int(si())
 
-        # min 값 갱신될 때마다 min_numbers 리스트도 갱신
-        if tmp_ans != ans :
-            min_numbers = tmp_list[:]
+# 현재 까지 색칠한 칸들의 숫자 저장
+permutation = []
+
+# n * n 행렬 입력 받는 방법
+# zero padding 필요
+# graph = [list(map(int, si().split()))] for _ in range(n)
+graph = [[0] * (n + 1)]
+for _ in range(n):
+    graph.append([0] + list(map(int, si().split())))
+
+visited = [[False for _ in range(n + 1)] for _ in range(n + 1)]
+
+min_num = - sys.maxsize
+
+
+def can_paint(r, c):
+    flag = True
     
-        # 원소는 다른데 합이 같은 경우 고려
-        if ans == sum(tmp_list) :
-            min_numbers2 = tmp_list[:]
-        return
+    # 같은 열에 색칠된 다른 칸이 있는지 체크
+    for i in range(1, n + 1):
+        if i != r and visited[i][c]:
+            flag = False
 
-    for i in range(n):
-        if visited[i] == True : continue
-        numbers.append(i)
-        visited[i] = True
-        find_max(row+1)
-        numbers.pop()
-        visited[i] = False
+    return flag
 
-find_max(0)
-min_numbers.sort(); min_numbers2.sort()
-print(max(min_numbers[0], min_numbers2[0]))
+
+# curr_num: 현재 색칠할 행
+def find_permutation(curr_num):
+    global min_num
+    # n 개의 칸을 모두 색칠 했을 때,
+    if curr_num == n + 1:
+        # 조건 2) 색칠된 칸에 적힌 수들 중 최솟값이 최대가 되도록
+        local_min_num = min(permutation)
+        min_num = max(min_num, local_min_num)
+
+    else:
+        # 1 부터 n * n 번째 칸 중 하나를 선택하여 색칠
+        for i in range(1, n + 1):
+            # 배열에 들어간 수가 이미 min_num 보다 작다면 나머지를 볼 필요가 없다.
+            if len(permutation) > 0 and min_num > min(permutation):
+                return
+            # 
+            # 조건 1) 각 행과 열을 정확히 1개만 색칠해야함
+            if not can_paint(curr_num, i):
+                continue
+            
+            visited[curr_num][i] = True
+            # 색칠한 칸에 적힌 값 추가
+            permutation.append(graph[curr_num][i])
+            # 다음 행으로 넘어감 
+            find_permutation(curr_num + 1)
+            permutation.pop()
+            visited[curr_num][i] = False
+            
+
+find_permutation(1)
+print(min_num)
