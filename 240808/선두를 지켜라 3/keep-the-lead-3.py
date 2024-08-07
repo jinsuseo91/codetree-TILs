007e129c42a47731f6d1888b77f0f9e9ee1a1e08
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-08 00:58:18 +0900

    Title: 선두를 지켜라 3, Time: 153ms, Memory: 38MB, Status: Wrong Answer - CodeTree

---
+++ 240808/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+a_d, b_d = [0], [0]
+
+def make_dist(player, p_dist):
+    for i in range(player):
+        velocity, time = map(int, input().split())
+        for _ in range(time):
+            p_dist.append(velocity + p_dist[-1])
+
+make_dist(n, a_d)
+make_dist(m, b_d)
+
+compare = [a_d[i] - b_d[i] for i in range(len(a_d))][1:]
+answer = 0
+
+for idx in range(1, len(compare)):
+    if compare[idx] * compare[idx - 1] <= 0:
+        answer += 1
+
+print(answer)
