8bbcff2c81fd525b073ac47d244dc5b0293ca3c4
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-08 15:56:44 +0900

    Title: 선두를 지켜라 3, Time: 151ms, Memory: 38MB, Status: Wrong Answer - CodeTree

--- 240808/선두를 지켜라 3/keep-the-lead-3.py
+++ 240808/선두를 지켜라 3/keep-the-lead-3.py
@@ -1,40 +1,20 @@
-n, m = map(int, input().split())
-a_moves, b_moves = [], []
+N, M = map(int, input().split())
+a_dist, b_dist = [0], [0]
 
-# A의 이동 명령을 입력받아 리스트로 저장
-for _ in range(n):
-    v, t = map(int, input().split())
-    a_moves.append((v, t))
+def make_dist(player, p_dist):
+    for i in range(player):
+        velocity, times = map(int, input().split())
+        for _ in range(times):
+            p_dist.append(velocity + p_dist[-1])
 
-# B의 이동 명령을 입력받아 리스트로 저장
-for _ in range(m):
-    v, t = map(int, input().split())
-    b_moves.append((v, t))
+make_dist(N, a_dist)
+make_dist(M, b_dist)
 
-# A와 B의 위치를 추적할 리스트 초기화
-a_pos, b_pos = [0], [0]
+compare = [a_dist[i] - b_dist[i] for i in range(len(a_dist))][1:]
+answer = 0
 
-# A의 이동을 시간별로 기록
-for v, t in a_moves:
-    for _ in range(t):
-        a_pos.append(a_pos[-1] + v)
+for idx in range(1, len(compare)):
+    if compare[idx] * compare[idx - 1] <= 0:
+        answer += 1
 
-# B의 이동을 시간별로 기록
-for v, t in b_moves:
-    for _ in range(t):
-        b_pos.append(b_pos[-1] + v)
-
-# 두 리스트의 길이를 맞춤
-max_length = max(len(a_pos), len(b_pos))
-a_pos += [a_pos[-1]] * (max_length - len(a_pos))
-b_pos += [b_pos[-1]] * (max_length - len(b_pos))
-
-# 교차점을 찾기 위한 변수 초기화
-crossings = 0
-
-# 두 사람이 동일한 위치에 있는지 확인
-for i in range(1, max_length):
-    if a_pos[i] == b_pos[i] or (a_pos[i] - b_pos[i]) * (a_pos[i - 1] - b_pos[i - 1]) < 0:
-        crossings += 1
-
-print(crossings)
+print(answer)
