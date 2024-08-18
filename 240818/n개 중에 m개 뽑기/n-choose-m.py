45e4b6b72a16e67e0c5d689cdf1e088076f8078d
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-18 22:47:28 +0900

    Title: n개 중에 m개 뽑기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240818/n개 중에 m개 뽑기/n-choose-m.py
+++ 240818/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,21 +1,11 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
+data = []
+for i in range(1, n+1):
+    data.append(i)
 
-ans = []
 
-def print_answer():
-    for i in ans:
-        print(i, end=' ')
-    print()
-
-def choose(curr_num, num):
-    if curr_num == m + 1:
-        print_answer()
-		    return
-    
-    for i in range(1, n + 1):
-        if i > num:
-            ans.append(i)
-            choose(curr_num + 1, i)
-            ans.pop()
-
-choose(1,0)
+result= list(combinations(data, m))
+for i in result:
+    print(*i)
