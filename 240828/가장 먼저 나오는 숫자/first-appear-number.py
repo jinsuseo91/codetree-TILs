38a275c5f85699dffa42c13523a1775614c68da1
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-08-28 18:01:41 +0900

    Title: 가장 먼저 나오는 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240828/가장 먼저 나오는 숫자/first-appear-number.py
+++ 240828/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,35 +1,22 @@
-# n, m = map(int, input().split())
-# arr = list(map(int, input().split()))
-# arr1 = list(map(int, input().split()))
-
-# def lower_bound(target):
-#     left, right = 0, n - 1
-#     min_idx = n
-#     while left <= right:
-#         mid = (left + right) // 2
-#         if arr[mid] >= target:
-#             min_idx = min(min_idx, mid)
-#             right = mid - 1
-#         else:
-#             left = mid + 1
-#     return min_idx
-
-# for i in arr1:
-#     if i not in arr:
-#         print(-1)
-#     else:
-#         x = lower_bound(i)
-#         print(x+1)
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr1 = list(map(int, input().split()))
 
-# 각 숫자의 첫 번째 등장 위치를 기록하는 딕셔너리
-position_map = {}
-for idx, num in enumerate(arr):
-    if num not in position_map:
-        position_map[num] = idx + 1  # 1-based index
+def lower_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    return min_idx
 
-# 각 질문에 대해 답변
-for x in arr1:
-    print(position_map.get(x, -1))
+for i in arr1:
+    lo = lower_bound(i)
+    if lo <= n and arr[lo] == i:
+        print(lo + 1)
+    else:
+        print(-1)
