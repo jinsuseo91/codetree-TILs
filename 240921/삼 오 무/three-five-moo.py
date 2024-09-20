45fef5f3124e0a31b9ddaca162b084d424d83631
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-09-21 00:07:10 +0900

    Title: 삼 오 무, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240921/삼 오 무/three-five-moo.py
@@ -0,0 +1,16 @@
+n = int(input())
+min_ = int(1e10)
+left, right = 1, min_
+
+def is_possible(k):
+    tot = k//3 + k//5 + k//15
+
+    return k - tot
+while left <= right:
+    mid = (left + right) // 2
+    if is_possible(mid) >= n:
+        right = mid - 1
+        min_ = min(min_, mid)
+    else:
+        left = mid + 1
+print(min_)
