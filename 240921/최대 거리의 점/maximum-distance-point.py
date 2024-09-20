4dd67022dea6b6442d0d25898a2d22190fa44602
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-09-21 00:46:33 +0900

    Title: 최대 거리의 점, Time: 194ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240921/최대 거리의 점/maximum-distance-point.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+def can_d(d):
+    cnt = 1
+    last_position = arr[0]
+
+    for i in range(1, n):
+        if arr[i] - last_position >= d:
+            cnt += 1
+            last_position = arr[i]
+        if cnt >= m:
+            return True
+    return False
+
+left, right = 1, arr[-1] - arr[0]
+ans = 0
+
+while left <= right:
+    mid = (left + right)//2
+    if can_d(mid):
+        ans = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+print(ans)
