eb87f2d28c794ed4c2dfdd68d3f700e00ae86528
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2024-10-08 10:54:59 +0900

    Title: 좌우 반전시키기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241008/좌우 반전시키기/flip-left-and-right.py
+++ 241008/좌우 반전시키기/flip-left-and-right.py
@@ -3,12 +3,17 @@ arr = list(map(int, input().split()))
 
 result = 0
 
-for i in range(n - 1):
-    if arr[i] == 0:
+# 두 번째 위치부터 마지막 위치 직전까지 검사
+for i in range(1, n):
+    # 현재 위치가 0이라면, 이 위치를 눌러야 합니다.
+    if arr[i - 1] == 0:
         result += 1
-        arr[i] = 1
-        arr[i + 1] = 1 - arr[i + 1]
-        #arr[i - 1] = 1 - arr[i - 1]
+        # 현재 위치와 그 다음 위치를 반전
+        arr[i - 1] = 1 - arr[i - 1]
+        if i < n:
+            arr[i] = 1 - arr[i]
+
+# 모든 값이 1인지 확인
 if all(arr):
     print(result)
 else:
