f8a042684ceaacf8c5423623950de84630a8a697
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-04-11 16:47:23 +0900

    Title: 고대 문명 유적 탐사, Time: 134ms, Memory: 20MB, Status: Passed - Codetree

--- 250411/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 250411/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -142,116 +142,201 @@
 #         result.append(total_score)
 
 # print(' '.join(map(str, result)))
+
+# import pprint
+# import collections
+
+# K, M = [int(x) for x in input().split()]
+# board = []
+# dx, dy = [1,0,-1,0], [0,1,0,-1]
+# answer = []
+
+# for _ in range(5):
+#     board.append([int(x) for x in input().split()])
+
+# walls = [int(x) for x in input().split()]
+# wall_idx = 0
+
+# def OOB(x, y):
+#     return x<0 or x>=5 or y<0 or y>=5
+
+# def rotate(x, y): 
+#     x, y = (x-1), (y-1)
+#     tmp_board = [[0] * 3 for _ in range(3)]
+#     for i in range(3):
+#         for j in range(3):
+#             tmp_board[i][j] = board[x+2-j][y+i]
+
+#     for i in range(3):
+#         for j in range(3):
+#             board[x+i][y+j] = tmp_board[i][j]
+
+# def bfs():
+
+#     queue = collections.deque([])
+#     vis = collections.defaultdict(int)
+#     paths = collections.defaultdict(list)
+#     treasures = set()
+
+#     for i in range(5):
+#         for j in range(5):
+#             if vis[(i, j)]: continue
+#             vis[(i, j)] = 1
+#             queue.append(((i, j), (i, j)))
+            
+#             while queue :
+#                 (x, y), (px, py) = queue.popleft()
+#                 paths[(px, py)].append((x, y))
+                
+#                 is_last = 1
+#                 for dir in range(4):
+#                     nx, ny = x+dx[dir], y+dy[dir]
+#                     if OOB(nx, ny) or vis[(nx, ny)] or board[x][y] != board[nx][ny]:
+#                         continue
+#                     is_last = 0
+#                     vis[(nx, ny)] = 1
+#                     queue.append(((nx, ny), (px, py)))
+                
+#                 if is_last and len(paths[(px, py)]) >= 3 :
+#                     for cur in paths[(px, py)] :
+#                         treasures.add(cur)
+    
+#     return list(treasures)
+
+
+
+# r, c, d, treasures = 5, 5, 4, []
+# while K :
+#     for i in range(1, 4):
+#         for j in range(1, 4):
+#             for new_d in range(3):
+#                 rotate(i, j)
+#                 new_treasures = bfs()
+#                 if [len(treasures), -d, -c, -r] < [len(new_treasures), -new_d, -j, -i] :
+#                     r, c, d = i ,j, new_d
+#                     treasures = new_treasures
+#             rotate(i, j)
+
+    
+#     if len(treasures) == 0 :
+#         break
+#     else :
+#         for _ in range(d+1):
+#             rotate(r, c)
+
+#     tmp_sum = 0
+#     while treasures : 
+#         tmp_sum += len(treasures)
+#         treasures.sort(key= lambda x: (x[1], -x[0])) 
+#         for (x, y) in treasures :
+#             board[x][y] = walls[wall_idx]
+#             wall_idx = (wall_idx + 1) % M
+#         treasures = bfs()
+
+#     answer.append(tmp_sum)
+#     K -= 1
+
+# print(*answer)
+
 from collections import deque
 
-N_large = 5  # 고대 문명 전체 격자 크기입니다.
-N_small = 3  # 회전시킬 격자의 크기입니다.
+N = 5
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
-# 고대 문명 격자를 정의합니다
-class Board:
-    def __init__(self):
-        self.a = [[0 for _ in range(N_large)] for _ in range(N_large)]
+def in_range(x, y):
+    return 0 <= x < N and 0 <= y < N
 
-    def in_range(self, y, x):
-        # 주어진 y, x가 고대 문명 격자의 범위안에 있는지 확인하는 함수 입니다.
-        return 0 <= y < N_large and 0 <= x < N_large
+def rotate(temp, angle):
+    if angle == 90:
+        return [list(reversed(col)) for col in zip(*temp)]
+    elif angle == 180:
+        return [row[::-1] for row in temp[::-1]]
+    elif angle == 270:
+        return [list(col) for col in zip(*temp)][::-1]
 
-    # 현재 격자에서 sy, sx를 좌측상단으로 하여 시계방향 90도 회전을 cnt번 시행했을때 결과를 return 합니다.
-    def rotate(self, sy, sx, cnt):
-        result = Board()
-        result.a = [row[:] for row in self.a]
-        for _ in range(cnt):
-            # sy, sx를 좌측상단으로 하여 시계방향 90도 회전합니다.
-            tmp = result.a[sy + 0][sx + 2]
-            result.a[sy + 0][sx + 2] = result.a[sy + 0][sx + 0]
-            result.a[sy + 0][sx + 0] = result.a[sy + 2][sx + 0]
-            result.a[sy + 2][sx + 0] = result.a[sy + 2][sx + 2]
-            result.a[sy + 2][sx + 2] = tmp
-            tmp = result.a[sy + 1][sx + 2]
-            result.a[sy + 1][sx + 2] = result.a[sy + 0][sx + 1]
-            result.a[sy + 0][sx + 1] = result.a[sy + 1][sx + 0]
-            result.a[sy + 1][sx + 0] = result.a[sy + 2][sx + 1]
-            result.a[sy + 2][sx + 1] = tmp
-        return result
+def rotate_grid(grid, x, y, angle):
+    temp = [row[y:y+3] for row in grid[x:x+3]]
+    rotated = rotate(temp, angle)
+    new_grid = [row[:] for row in grid]
+    for i in range(3):
+        for j in range(3):
+            new_grid[x+i][y+j] = rotated[i][j]
+    return new_grid
 
-    # 현재 격자에서 유물을 획득합니다.
-    # 새로운 유물 조각을 채우는것은 여기서 고려하지 않습니다.
-    def cal_score(self):
-        score = 0
-        visit = [[False for _ in range(N_large)] for _ in range(N_large)]
-        dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+def find_artifacts(grid):
+    visited = [[False] * N for _ in range(N)]
+    found = set()
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] or grid[i][j] == 0:
+                continue
+            q = deque()
+            group = []
+            q.append((i, j))
+            group.append((i, j))
+            visited[i][j] = True
+            while q:
+                x, y = q.popleft()
+                for d in range(4):
+                    nx, ny = x + dx[d], y + dy[d]
+                    if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny] == grid[x][y]:
+                        visited[nx][ny] = True
+                        q.append((nx, ny))
+                        group.append((nx, ny))
+            if len(group) >= 3:
+                found.update(group)
+    return list(found)
 
-        for i in range(N_large):
-            for j in range(N_large):
-                if not visit[i][j]:
-                    # BFS를 활용한 Flood Fill 알고리즘을 사용하여 visit 배열을 채웁니다.
-                    # 이때 trace 안에 조각들의 위치가 저장됩니다.
-                    q, trace = deque([(i, j)]), deque([(i, j)])
-                    visit[i][j] = True
-                    while q:
-                        cur = q.popleft()
-                        for k in range(4):
-                            ny, nx = cur[0] + dy[k], cur[1] + dx[k]
-                            if self.in_range(ny, nx) and self.a[ny][nx] == self.a[cur[0]][cur[1]] and not visit[ny][nx]:
-                                q.append((ny, nx))
-                                trace.append((ny, nx))
-                                visit[ny][nx] = True
-                    # 위에서 진행된 Flood Fill을 통해 조각들이 모여 유물이 되고 사라지는지 확인힙니다.
-                    if len(trace) >= 3:
-                        # 유물이 되어 사라지는 경우 가치를 더해주고 조각이 비어있음을 뜻하는 0으로 바꿔줍니다.
-                        score += len(trace)
-                        while trace:
-                            t = trace.popleft()
-                            self.a[t[0]][t[1]] = 0
-        return score
+def refill_grid(grid, artifacts, walls, wall_idx):
+    artifacts.sort(key=lambda x: (x[1], -x[0]))  # 열 오름차순, 행 내림차순
+    for x, y in artifacts:
+        grid[x][y] = walls[wall_idx % len(walls)]
+        wall_idx += 1
+    return wall_idx
 
-    # 유물 획득과정에서 조각이 비어있는 곳에 새로운 조각을 채워줍니다.
-    def fill(self, que):
-        # 열이 작고 행이 큰 우선순위로 채워줍니다.
-        for j in range(N_large):
-            for i in reversed(range(N_large)):
-                if self.a[i][j] == 0:
-                    self.a[i][j] = que.popleft()
+# ✅ 입력
+K, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+walls = list(map(int, input().split()))
 
-def main():
-    # 입력을 받습니다.
-    K, M = map(int, input().split())
-    board = Board()
-    for i in range(N_large):
-        board.a[i] = list(map(int, input().split()))
-    q = deque()
-    for t in list(map(int, input().split())):
-        q.append(t)
+wall_idx = 0
+result = []
 
-    # 최대 K번의 탐사과정을 거칩니다.
-    for _ in range(K):
-        maxScore = 0
-        maxScoreBoard = None
-        # 회전 목표에 맞는 결과를 maxScoreBoard에 저장합니다.
-        # (1) 유물 1차 획득 가치를 최대화
-        # (2) 회전한 각도가 가장 작은 방법을 선택
-        # (3) 회전 중심 좌표의 열이 가장 작은 구간을, 그리고 열이 같다면 행이 가장 작은 구간을 선택
-        for cnt in range(1, 4):
-            for sx in range(N_large - N_small + 1):
-                for sy in range(N_large - N_small + 1):
-                    rotated = board.rotate(sy, sx, cnt)
-                    score = rotated.cal_score()
-                    if maxScore < score:
-                        maxScore = score
-                        maxScoreBoard = rotated
-        # 회전을 통해 더 이상 유물을 획득할 수 없는 경우 탐사를 종료합니다.
-        if maxScoreBoard is None:
+while K:
+    best_score = -1
+    best_grid = None
+    best_angle = best_x = best_y = 999
+    best_artifacts = []
+
+    for x in range(N - 2):
+        for y in range(N - 2):
+            for angle in [90, 180, 270]:
+                rotated = rotate_grid(grid, x, y, angle)
+                artifacts = find_artifacts(rotated)
+                score = len(artifacts)
+                if (score > best_score or
+                    (score == best_score and angle < best_angle) or
+                    (score == best_score and angle == best_angle and y < best_y) or
+                    (score == best_score and angle == best_angle and y == best_y and x < best_x)):
+                    best_score = score
+                    best_grid = rotate_grid(grid, x, y, angle)
+                    best_angle, best_x, best_y = angle, x, y
+                    best_artifacts = artifacts
+
+    if best_score == 0:
+        break
+
+    grid = best_grid
+    total = 0
+
+    while True:
+        total += len(best_artifacts)
+        wall_idx = refill_grid(grid, best_artifacts, walls, wall_idx)
+        best_artifacts = find_artifacts(grid)
+        if not best_artifacts:
             break
-        board = maxScoreBoard
-        # 유물의 연쇄 획득을 위해 유물 조각을 채우고 유물을 획득하는 과정을 더이상 획득할 수 있는 유물이 없을때까지 반복합니다.
-        while True:
-            board.fill(q)
-            newScore = board.cal_score()
-            if newScore == 0:
-                break
-            maxScore += newScore
 
-        print(maxScore, end=" ")
+    result.append(total)
+    K -= 1
 
-if __name__ == '__main__':
-    main()
+print(*result)
