f6f2bd5062693ed8dd54f7c1831d885fe4ae3be8
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-04-12 16:00:10 +0900

    Title: 마법의 숲 탐색, Time: 227ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 250412/마법의 숲 탐색/magical-forest-exploration.py
@@ -0,0 +1,62 @@
+R, C, K = map(int, input().split())
+unit = [list(map(int, input().split())) for _ in range(K)]
+arr = [[1] + [0] * C + [1] for _ in range(R + 3)] + [[1] * (C + 2)]
+
+exit_set = set() #출구 좌표
+
+di = [-1, 0, 1, 0]
+dj = [0, 1, 0, -1]
+
+def bfs(si, sj):
+    q = []
+    visited = [[False] * (C + 2) for _ in range(R + 4)]
+    max_ = 0 #-2해서 return !! 쵀대 행값
+
+    q.append((si, sj))
+    visited[si][sj] = True
+
+    while q:
+        ci, cj = q.pop(0)
+        max_ = max(max_, ci)
+
+        for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci + dx, cj + dy
+            #방문하지 않았거나, 내 칸이거나, 현재 위치가 탈출구인데 다음칸이 골렘이라면
+            if not visited[ni][nj] and (arr[ci][cj] == arr[ni][nj] or ((ci, cj) in exit_set and arr[ni][nj] > 1)):
+                q.append((ni, nj))
+                visited[ni][nj] = True
+    return max_ - 2
+
+ans = 0
+num = 2
+for cj, dr in unit:
+    ci = 1
+    while True:
+        if arr[ci + 1][cj - 1] + arr[ci + 2][cj] + arr[ci + 1][cj + 1] == 0:
+            ci += 1
+        elif (arr[ci - 1][cj - 1] + arr[ci][cj - 2] + arr[ci + 1][cj - 1] + arr[ci + 1][cj - 2] + arr[ci + 2][cj - 1]) == 0:
+            ci += 1
+            cj -= 1
+            dr = (dr - 1) % 4
+        elif (arr[ci - 1][cj + 1] + arr[ci][cj + 2] + arr[ci + 1][cj + 1] + arr[ci + 1][cj + 2] + arr[ci + 2][cj + 1]) == 0:
+            ci += 1
+            cj += 1
+            dr = (dr + 1) % 4
+        else:
+            break
+
+    if ci < 4:
+        arr = [[1] + [0] * C + [1] for _ in range(R + 3)] + [[1] * (C + 2)]
+        exit_set = set()
+        num = 2
+
+    else: #골렘 표시, 탈출구 위치추가
+        arr[ci + 1][cj] = arr[ci - 1][cj] = num
+        arr[ci][cj - 1:cj + 2] = [num] * 3
+        num += 1
+
+        exit_set.add((ci + di[dr], cj + dj[dr]))
+
+        ans += bfs(ci, cj)
+
+print(ans)
