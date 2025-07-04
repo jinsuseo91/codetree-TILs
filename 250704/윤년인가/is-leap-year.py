851bad4f685bdfe6e0fc567848bf06f383e80852
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-07-04 21:21:10 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/윤년인가/is-leap-year.py
+++ 250704/윤년인가/is-leap-year.py
@@ -1,7 +1,12 @@
 y = int(input())
 
 if y % 4 == 0:
-    if y % 100 == 0 and y % 400 != 0:
-        print("false")
+    if y % 100 == 0:
+        if y % 400 == 0:
+            print("true")
+        else:
+            print("false")
     else:
         print("true")
+else:
+    print("false")
