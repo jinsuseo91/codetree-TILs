07c995d489f98bcb2c34168e9d198e18d4d2d1d3
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-07-07 15:29:46 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250707/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return False
+    return True
+cnt = 0
+def num(n):
+    if is_prime(n):
+        if ((n // 10) + (n % 10)) % 2 == 0:
+            return True
+    return False
+
+for i in range(a, b + 1):
+    if num(i):
+        cnt += 1
+
+print(cnt)
