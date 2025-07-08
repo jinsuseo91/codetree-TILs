d145ba20f30ad6bc5d62901556c1f7f49a0e2a70
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-07-08 16:19:06 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250708/DateTime to DateTime/datetime-to-datetime.py
+++ 250708/DateTime to DateTime/datetime-to-datetime.py
@@ -1,9 +1,16 @@
 a, b, c = map(int, input().split())
 
-result = (a - 11) * 24 * 60
-num1 = b * 60 + c
-num2 = 11 * 60 + 11
+start_day = 11
+start_hour = 11
+start_minute = 11
 
-result += (num1 - num2)
+start_total = start_day * 24 * 60 + start_hour * 60 + start_minute
 
-print(result)
+target_total = a * 24 * 60 + b * 60 + c
+
+diff = target_total - start_total
+
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
