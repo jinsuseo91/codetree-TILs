da4e26ec4879c2a8fe8c9fd79b926fd49383065c
jinsuseo91 <165881043+jinsuseo91@users.noreply.github.com>
2025-07-09 18:29:16 +0900

    Title: 연속되는 수 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250709/연속되는 수 2/continuous-number2.py
+++ 250709/연속되는 수 2/continuous-number2.py
@@ -1,10 +1,10 @@
 n = int(input())
-arr = [list(input()) for _ in range(n)]
+arr = [int(input()) for _ in range(n)]
 
 cnt = 1
 
-for i in range(len(arr) - 1):
-    if arr[i] != arr[i + 1]:
+for i in range(1, n):
+    if arr[i] != arr[i - 1]:
         cnt += 1
 
 print(cnt)
